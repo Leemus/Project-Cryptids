01563106f38f7af1993df5e809fcc82464ab941d
Liam <c15526297@mydit.ie>
2018-09-28 17:47:39 +0100

    First official push, includes third person character moving with cinemachine camera, Camera switching with E and Q and photos save to screenshots folder.

---
+++ Cryptid Final/Assets/CamSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour {
+
+    public Camera mainCam;
+    public Camera photoCam;
+    // Use this for initialization
+    void Start()
+    {
+        mainCam.enabled = true;
+        photoCam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Enable photo camera and disable main camera.
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            mainCam.enabled = false;
+            photoCam.enabled = true;
+        }
+
+        //Enable main camera and disable photo camera.
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mainCam.enabled = true;
+            photoCam.enabled = false;
+
+        }
+    }
+}
---
+++ Cryptid Final/Assets/Capture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Capture : MonoBehaviour {
+    public int Width = 2550;
+    public int Height = 3300;
+    public Camera camera;
+    private bool photocap = false;
+    
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/CameraPhoto{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        photocap = true;
+    }
+
+    void LateUpdate()
+    {
+        photocap |= Input.GetMouseButtonDown(0);
+        if (photocap)
+        {
+            RenderTexture rt = new RenderTexture(Width, Height, 24);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(Width, Height, TextureFormat.RGB24, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, Width, Height), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(Width, Height);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            photocap = false;
+        }
+    }
+}
