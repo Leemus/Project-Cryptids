1c750d268df760a14fd18b08c39fa91dd69be2ae
Liam <c15526297@mydit.ie>
2018-09-30 16:29:59 +0100

    More and more backend for Inventory, UI Image becomes Photo image.

--- Cryptid Final/Assets/AddtoInventory.cs
+++ Cryptid Final/Assets/AddtoInventory.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class AddtoInventory : MonoBehaviour {
-    public Canvas store;
+   
+    public RawImage[] canvas; 
     public Texture[] photos;
     public int index;
 	// Use this for initialization
@@ -14,6 +15,21 @@ public class AddtoInventory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        AddtoCanvas();
         photos[0] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png",  typeof(Texture));
+        photos[1] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png", typeof(Texture));
+        photos[2] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png", typeof(Texture));
+        photos[3] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png", typeof(Texture));
+        photos[4] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png", typeof(Texture));
+        photos[5] = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/screenshots/photo0.png", typeof(Texture));
+    }
+
+    void AddtoCanvas()
+    {
+        
+         canvas[0].texture = photos[0];
+       // Debug.Log("me");
+        
+
     }
 }
