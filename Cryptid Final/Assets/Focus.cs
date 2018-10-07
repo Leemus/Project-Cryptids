a16decc72c08d666b8d81f83f006d92c2e01a068
Liam <c15526297@mydit.ie>
2018-10-07 17:58:43 +0100

    Starting to put in focusing yo

---
+++ Cryptid Final/Assets/Focus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.PostProcessing;
+using UnityEngine;
+
+public class Focus : MonoBehaviour {
+    public PostProcessingProfile cam;
+    float val;
+    bool down = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        var dof = cam.depthOfField.settings;
+        dof.focusDistance = val;
+        cam.depthOfField.settings = dof;
+      
+        if (down == true && scroll > 0)
+        {
+           
+            val += 0.1f;
+
+        }
+
+        if(down == true & scroll < 0)
+        {
+            val -= 0.1f;
+
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            down = true;
+
+        }
+        else
+        {
+            down = false;
+        }
+    }
+
+   
+}
