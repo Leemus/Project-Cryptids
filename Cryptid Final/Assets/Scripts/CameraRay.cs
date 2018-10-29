d3b8cd182c56e708b09fb978eddf7a7867babda6
Liam <c15526297@mydit.ie>
2018-10-29 17:19:26 +0000

    RAVEcast11!!!

---
+++ Cryptid Final/Assets/Scripts/CameraRay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRay : MonoBehaviour {
+  //  public RaycastHit hitr;
+  //  public GameObject test;
+	public GameObject topLeftOr;
+
+	public GameObject topRightOr;
+
+	public GameObject botLeftOr;
+
+	public GameObject botRightOr;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+		Debug.DrawRay(topLeftOr.transform.position, forward, Color.green);
+		Debug.DrawRay(topRightOr.transform.position, forward, Color.blue);
+		Debug.DrawRay(botLeftOr.transform.position, forward, Color.yellow);
+		Debug.DrawRay(botRightOr.transform.position, forward, Color.magenta);
+
+
+	}
+}
