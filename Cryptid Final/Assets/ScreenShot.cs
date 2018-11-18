8b357680b894628013ba1bc6eef741fb10c6ff4e
Liam <c15526297@mydit.ie>
2018-11-18 15:23:11 +0000

    Journal re-worked for tomorrow

--- Cryptid Final/Assets/ScreenShot.cs
+++ Cryptid Final/Assets/ScreenShot.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class ScreenShot : MonoBehaviour
 {
 	public RawImage photos,photos1,photos2,photos3,photos4,photos5;
-	public Texture2D image;
+	public bool haveP, haveP1, haveP2, haveP3, haveP4, haveP5;
+	//public Texture2D image;
+	public bool camOn = false;
+	public int me;
 	
 	IEnumerator ScreenCap()
 	{
@@ -14,33 +17,66 @@ public class ScreenShot : MonoBehaviour
 
 		yield return new WaitForEndOfFrame();
 		var texture = ScreenCapture.CaptureScreenshotAsTexture();
-		photos.texture = texture;
+		
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(DevShop.goodbye);
-		/*if (Input.GetMouseButton(0) && DevShop.goodbye == 0)
+		Debug.Log(haveP1);
+		if (Input.GetMouseButtonDown(0) && haveP == true && camOn == true)
 		{
-
-			DevShop.goodbye += 1;
 			photos.texture = ScreenCapture.CaptureScreenshotAsTexture();
+			haveP = false;
+			me += 1;
+
+		}
+
+		if (Input.GetMouseButtonUp(0) && haveP == false && me == 1)
+		{
+			haveP1 = true;
+
 		}
 		
-		if (Input.GetMouseButton(0) && DevShop.goodbye == 1)
+		if (Input.GetMouseButtonDown(0) && haveP1 == true && camOn == true)
 		{
-
-			DevShop.goodbye += 1;
+			haveP1 = false;
+			me += 1;
+                          	//		haveP2 = true;
 			photos1.texture = ScreenCapture.CaptureScreenshotAsTexture();
 		}
-		if (Input.GetMouseButton(0) && DevShop.goodbye == 2)
-		{
 
-			DevShop.goodbye += 1;
+		if (Input.GetMouseButtonUp(0) && haveP1 == false && me == 2)
+		{
+			haveP2 = true;
+		}
+		
+		if (Input.GetMouseButtonUp(0) && haveP2 == true && camOn == true)
+		{
+			//haveP2 = false;
+
 			photos2.texture = ScreenCapture.CaptureScreenshotAsTexture();
 		}
-		*/
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			camOn = true ;
+
+
+
+		}
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			camOn = false;
+
+
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			camOn = false;
+
+		}
 	}
 	
 	
